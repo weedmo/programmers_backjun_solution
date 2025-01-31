670157dbb9ba4cbca1948ee5c3470b56958c92d2
weedmo <89618700+weedmo@users.noreply.github.com>
2025-01-31 14:08:58 +0900

    [level 0] Title: 약수 구하기, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = []
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            answer.append(i)
+            if i != n // i:  
+                answer.append(n // i)
+
+    return sorted(answer)
